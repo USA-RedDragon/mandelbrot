5b8d2f4187fa896aad28797228c886dd73f865a3
Jacob McSwain <jacob@mcswain.dev>
2025-02-28 09:32:11 -0600

    mandelbrot: fix color scale

--- internal/mandelbrot/color.go
+++ internal/mandelbrot/color.go
@@ -1,6 +1,14 @@
 package mandelbrot
 
+import (
+	"math"
+)
+
+var ()
+
 func (m *Mandelbrot) color(n uint64) [4]byte {
-	color := uint8(255 - (n * 255 / m.maxIterations))
+	factor := math.Sqrt(float64(n) / float64(m.maxIterations))
+	intensity := math.Round(float64(m.maxIterations) * factor)
+	color := uint8(intensity * 255 / float64(m.maxIterations))
 	return [4]byte{color, color, color, 255}
 }
